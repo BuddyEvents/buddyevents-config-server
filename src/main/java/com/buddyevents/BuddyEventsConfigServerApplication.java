439f1f3f97f33b019fa64761c64f923c26f013f3
buddyeventcoder <eml2syam@gmail.com>
2016-12-18 09:13:11 -0500

    pointing to git for config changes

--- src/main/java/com/buddyevents/BuddyEventsConfigServerApplication.java
+++ src/main/java/com/buddyevents/BuddyEventsConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.buddyevents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class BuddyEventsConfigServerApplication {
 
 	public static void main(String[] args) {
